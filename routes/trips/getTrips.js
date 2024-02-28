829352d6026562aaa0c6d04be65fd7929af60984
Sherif Elkereti <101013434+Smohamed10@users.noreply.github.com>
2024-02-28 22:49:32 +0200

    FIX GET TRIPS

--- routes/trips/getTrips.js
+++ routes/trips/getTrips.js
@@ -3,18 +3,10 @@ const { body } = require("express-validator");
 const connection = require("../../db/dbConnection");
 const util = require("util");
 
-router.get("/", body("category"), async (req, res) => {
+router.get("/",body("category"), async (req, res) => {
   try {
     const query = util.promisify(connection.query).bind(connection);
-    let trips;
-
-    if (req.body.category === "All") {
-      // If category is "All", fetch all trips
-      trips = await query("SELECT * FROM trip");
-    } else {
-      // Otherwise, fetch trips with the specified category
-      trips = await query("SELECT * FROM trip WHERE category = ?", req.body.category);
-    }
+    const trips = await query("SELECT * FROM trip WHERE category = ?",req.body.category);
 
     if (trips.length === 0) {
       return res.status(404).json("There Is No Images To Show");
@@ -37,4 +29,4 @@ router.get("/", body("category"), async (req, res) => {
   }
 });
 
-module.exports = router;
+module.exports = router;
